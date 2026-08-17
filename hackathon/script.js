2247f1c9691e4cb0fad4f06456b8358ee4a4dfb2
dhruvasrinivas-lol <ggdhruva@gmail.com>
2026-08-17 11:37:31 +0530

    Add files via upload

---
+++ hackathon/script.js
@@ -0,0 +1,47 @@
+let totalComplaints = 20;
+console.log(totalComplaints);
+
+const title = document.getElementById("dashboard-title");
+
+console.log(title);
+
+title.textContent = "Smart Complaint Manager";
+
+const complaints = [
+    {
+        id: 1,
+        title: "WiFi not working",
+        category: "WiFi",
+        location: "Hostel A",
+        status: "Pending"
+    },
+
+    {
+        id: 2,
+        title: "Broken fan",
+        category: "Maintenance",
+        location: "Hostel B",
+        status: "In Progress"
+    },
+
+    {
+        id: 3,
+        title: "Bad food quality",
+        category: "Mess",
+        location: "Main Mess",
+        status: "Resolved"
+    }
+];
+
+for (let i = 0; i < complaints.length; i++) {
+    console.log(complaints[i].title);
+}
+
+const pendingComplaints = complaints.filter(
+    complaint => complaint.status === "Pending"
+);
+console.log(pendingComplaints);
+
+const titles = complaints.map(
+    complaint => complaint.title
+);
